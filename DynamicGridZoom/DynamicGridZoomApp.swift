53691a91910533be8b1c9362d38fb9d3a466fb58
Alex Marchant <alex_marchant@outlook.com>
2023-06-06 00:29:15 +0200

    Initial Commit

---
+++ DynamicGridZoom/DynamicGridZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicGridZoomApp.swift
+//  DynamicGridZoom
+//
+//  Created by Alex Marchant on 06/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct DynamicGridZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
